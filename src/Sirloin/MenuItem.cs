43a4a2d21c3e68cb863c10aa440091c48e6a2275
James Ko <jamesqko@gmail.com>
2016-01-31 16:38:57 -0500

    Add Symbol and SymbolText APIs to MenuItem

--- src/Sirloin/MenuItem.cs
+++ src/Sirloin/MenuItem.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
 
 namespace Sirloin
 {
     public sealed class MenuItem
     {
         public string Label { get; set; }
-        public string Symbol { get; set; }
         public Type DestPage { get; set; }
         public object Argument { get; set; }
+        public string SymbolText { get; set; }
+
+        public Symbol Symbol
+        {
+            get
+            {
+                var text = this.SymbolText;
+                if (string.IsNullOrEmpty(text))
+                    return default(Symbol);
+
+                char first = text[0];
+                return (Symbol)first;
+            }
+            set
+            {
+                char c = (char)value;
+                this.SymbolText = c.ToString();
+            }
+        }
     }
 }
